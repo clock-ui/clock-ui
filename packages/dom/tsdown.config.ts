7a67790b30d0c93ba6e165afbaef0d4c42ee28ce
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2025-11-11 02:32:28 +0800

    fix: remove shared config, apply tsdown config on each packages

--- packages/dom/tsdown.config.ts
+++ packages/dom/tsdown.config.ts
@@ -1,7 +1,26 @@
-import { withCSSExportConfig } from "config/tsdown";
+import LightningCSS from "unplugin-lightningcss/rolldown";
+import { defineConfig } from "tsdown";
 
-export default withCSSExportConfig({
+export default defineConfig({
   globalName: "ClockUI",
   format: ["esm", "umd"],
   dts: true,
+
+  minify: true,
+  clean: true,
+  exports: {
+    customExports(exports) {
+      exports["./base.css"] = "./dist/index.css";
+      return exports;
+    },
+  },
+  target: "chrome100",
+  noExternal: [/^@clock-ui\/.*/],
+  plugins: [
+    LightningCSS({
+      options: {
+        minify: true,
+      },
+    }),
+  ],
 });
--- packages/vue/tsdown.config.ts
+++ packages/vue/tsdown.config.ts
@@ -1,8 +1,27 @@
-import { withCSSExportConfig } from "config/tsdown";
+import { defineConfig } from "tsdown";
+import LightningCSS from "unplugin-lightningcss/rolldown";
 
-export default withCSSExportConfig({
+export default defineConfig({
   platform: "neutral",
   fromVite: true,
   format: ["esm", "cjs"],
   dts: { vue: true },
+
+  minify: true,
+  clean: true,
+  exports: {
+    customExports(exports) {
+      exports["./base.css"] = "./dist/index.css";
+      return exports;
+    },
+  },
+  target: "chrome100",
+  noExternal: [/^@clock-ui\/.*/],
+  plugins: [
+    LightningCSS({
+      options: {
+        minify: true,
+      },
+    }),
+  ],
 });
