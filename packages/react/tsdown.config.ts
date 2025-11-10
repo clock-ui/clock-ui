7321f602f19ded509eafd729d36f465ed02c4930
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2025-11-11 02:32:28 +0800

    fix: remove shared config, apply tsdown config on each packages

--- packages/react/tsdown.config.ts
+++ packages/react/tsdown.config.ts
@@ -1,6 +1,25 @@
-import { withCSSExportConfig } from "config/tsdown";
+import LightningCSS from "unplugin-lightningcss/rolldown";
+import { defineConfig } from "tsdown";
 
-export default withCSSExportConfig({
+export default defineConfig({
   format: ["esm", "cjs"],
   dts: true,
+
+  minify: true,
+  clean: true,
+  exports: {
+    customExports(exports) {
+      exports["./base.css"] = "./dist/index.css";
+      return exports;
+    },
+  },
+  target: "chrome100",
+  noExternal: [/^@clock-ui\/.*/],
+  plugins: [
+    LightningCSS({
+      options: {
+        minify: true,
+      },
+    }),
+  ],
 });
